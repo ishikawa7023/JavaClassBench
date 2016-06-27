41cc36f299bf4c24337454439c9ef65e847399bd
Yuki Ishikawa <7023.yu.ki@gmail.com>
2016-06-27 18:55:20 +0900

    CompletedClassBenchToZOM

--- ClassBenchToZOM.java
+++ ClassBenchToZOM.java
@@ -4,51 +4,49 @@ import java.util.*;
 
 public class ClassBenchToZOM {
     public static void main(String[] args){
-
+	
+	if (args.length != 2) {
+	    System.out.println("Arguments Error!\nUsage: $ java ClassBenchToZOM <rulelist> <outputfile>");
+	    //printf("Arguments Error!\nUsage: $ ./a.out <rulelist> <packetlist>.\n");
+	    System.exit(1);
+	}
 	try{
 	    List<String> origin_list = new ArrayList<String>();
-	    
 	    File input = new File(args[0]);
 	    BufferedReader br = new BufferedReader(new FileReader(input));
-	    //File output = new FIle(args[2]);
-	    //BufferedReader o_file = new BufferedReader(new FileReader(output));
-
+	    File output = new File(args[1]);
+	    BufferedWriter bw = new BufferedWriter(new FileWriter(output));
 	    String str;
 	    while((str = br.readLine()) != null){
 		List<String> slist = new ArrayList<String>();
 		List<String> dlist = new ArrayList<String>();
-	     
-		String[] result = str.split("\\t");
+		String[] result = str.split("\\s+|\\t");
 		StringBuilder sb = new StringBuilder(result[0]);
 		sb.deleteCharAt(0);
 		result[0]=sb.toString();
 		String SA = CIDRToZOM(result[0]);
 		String DA = CIDRToZOM(result[1]);
-		
-		String[] ssport = result[2].split("\\s:\\s");
-		int[] sport = {Integer.parseInt(ssport[0]),Integer.parseInt(ssport[1])};
-		String[] sdport = result[3].split("\\s:\\s");
-		int[] dport = {Integer.parseInt(sdport[0]),Integer.parseInt(sdport[1])};
-
-		slist = RangeToZOM.rangeTozom(sport[0],sport[1],16);
-		dlist = RangeToZOM.rangeTozom(dport[0],dport[1],16);
-		
-		String promask = prmsTozom(result[4]);
-		String flagmask = fgmsTozom(result[5]);
+		int[] sport = {Integer.parseInt(result[2]),Integer.parseInt(result[4])};
+		int[] dport = {Integer.parseInt(result[5]),Integer.parseInt(result[7])};
+		slist = RangeToZOM.rangeTozom(16,0,65535,sport[0],sport[1]);
+		dlist = RangeToZOM.rangeTozom(16,0,65535,dport[0],dport[1]);
+		String promask = prmsTozom(result[8]);
+		String flagmask = fgmsTozom(result[9]);
 		
 		for(String sp : slist){
 		    for(String dp : dlist){
-			origin_list.add(SA + DA + sp + dp + promask + flagmask);
+			origin_list.add(SA +" "+ DA +" "+ sp +" "+ dp +" "+ promask +" "+ flagmask);
 		    }
 		}		
 		
 	    }
-
-	    br.close();
-
 	    for(String ZOM : origin_list){//0,1,*のリストの表示
-		System.out.println(ZOM);
+		//System.out.println(ZOM);
+		bw.write(ZOM);
+		bw.newLine();
 	    }
+	    br.close();
+	    bw.close();
 	}catch(FileNotFoundException e){
 	    System.out.println(e);
 	}catch(IOException e){
@@ -59,13 +57,13 @@ public class ClassBenchToZOM {
     public static String prmsTozom(String promask){
 	
 	String[] pm = promask.split("/");
-	return (tenTotwo(Integer.decode(pm[1]),8) + tenTotwo(Integer.decode(pm[3]),8));
+	return (tenTotwo(Integer.decode(pm[0]),8) + tenTotwo(Integer.decode(pm[1]),8));
 
     }
     public static String fgmsTozom(String flagmask){
 
 	String[] fm = flagmask.split("/");
-	return (tenTotwo(Integer.decode(fm[1]),16) + tenTotwo(Integer.decode(fm[3]),16));
+	return (tenTotwo(Integer.decode(fm[0]),16) + tenTotwo(Integer.decode(fm[1]),16));
 
     }
 
